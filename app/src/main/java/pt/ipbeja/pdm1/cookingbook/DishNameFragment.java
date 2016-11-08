84e3ae3b21dc34a5cf58cbbed448c07dc19432af
Bruno <15464@alunos.ipbeja.pt>
2016-11-08 12:14:05 +0000

    Ponto 8

--- app/src/main/java/pt/ipbeja/pdm1/cookingbook/DishNameFragment.java
+++ app/src/main/java/pt/ipbeja/pdm1/cookingbook/DishNameFragment.java
@@ -1,6 +1,7 @@
 package pt.ipbeja.pdm1.cookingbook;
 
 
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.app.ListFragment;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -9,6 +10,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 
@@ -41,16 +43,36 @@ public class DishNameFragment extends ListFragment {
 
         //Toast.makeText(getActivity().getApplicationContext(), "" + Dish.DishName[position], Toast.LENGTH_SHORT).show();
 
-        if (getActivity().findViewById(R.id.activity_main) != null){
-            Toast.makeText(getActivity().getBaseContext(), "Clicked Portrait." + Dish.DishName[position],
-                    Toast.LENGTH_SHORT).show();
-        }
-        else{
-            Toast.makeText(getActivity().getBaseContext(), "Clicked Landscape." + Dish.DishName[position],
-                    Toast.LENGTH_SHORT).show();
+//        if (getActivity().findViewById(R.id.activity_main) != null){
+//            Toast.makeText(getActivity().getBaseContext(), "Clicked Portrait." + Dish.DishName[position],
+//                    Toast.LENGTH_SHORT).show();
+//        }
+//        else{
+//            Toast.makeText(getActivity().getBaseContext(), "Clicked Landscape." + Dish.DishName[position],
+//                    Toast.LENGTH_SHORT).show();
+//        }
+
+        if (getActivity().findViewById(R.id.activity_main) != null) {
+
+            DishDescriptionFragment newFragment = new DishDescriptionFragment();
+            Bundle args = new Bundle();
+            args.putInt("position", position);
+            newFragment.setArguments(args);
+            FragmentTransaction transaction = getActivity().getSupportFragmentManager().beginTransaction();
+            // Replace whatever is in the fragment_container view with this fragment,
+            // and add the transaction to the back stack so the user can navigate back
+            transaction.replace(R.id.activity_main, newFragment);
+            transaction.addToBackStack(null);
+
+            // Commit the transaction
+            transaction.commit();
+
+        } else {
+
+            TextView article = (TextView) getActivity().findViewById(R.id.txtDishDescription);
+            article.setText(Dish.DishDescription[position]);
         }
     }
-
 
 
 }
