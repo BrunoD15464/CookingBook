8700cc341a0ae5f2211b531c42955f1bfe545c6e
Bruno <15464@alunos.ipbeja.pt>
2016-11-08 11:42:46 +0000

    Ponto 4

--- app/src/main/java/pt/ipbeja/pdm1/cookingbook/MainActivity.java
+++ app/src/main/java/pt/ipbeja/pdm1/cookingbook/MainActivity.java
@@ -2,6 +2,7 @@ package pt.ipbeja.pdm1.cookingbook;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+        if (findViewById(R.id.activity_main) != null) {
+
+            // However, if we're being restored from a previous state,
+            // then we don't need to do anything and should return or else
+            // we could end up with overlapping fragments.
+            if (savedInstanceState != null) {
+                return;
+            }
+
+            // Create a new Fragment to be placed in the activity layout
+            Toast.makeText(this, "onCreate", Toast.LENGTH_SHORT).show();
+            DishNameFragment firstFragment = new DishNameFragment();
+
+            // In case this activity was started with special instructions from an
+            // Intent, pass the Intent's extras to the fragment as arguments
+            firstFragment.setArguments(getIntent().getExtras());
+
+            // Add the fragment to the 'fragment_container' FrameLayout
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.activity_main, firstFragment).commit();
+        }
     }
+
+
 }
